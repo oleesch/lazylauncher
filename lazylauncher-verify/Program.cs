1e90fe1ade05d360c862e9a37df4cb8698cc0873
Oliver Schweizer <oschweizer@me.com>
2019-05-17 21:03:56 +0200

    add small verify app

---
+++ lazylauncher-verify/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace lazylauncher_verify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string rawPath in args)
+            {
+                string path = Path.GetFullPath(Environment.ExpandEnvironmentVariables(rawPath));
+                if (File.Exists(path))
+                {
+                    Console.WriteLine($"Found file: {path}");
+                }
+                else if (Directory.Exists(path))
+                {
+                    Console.WriteLine($"Found directory: {path}");
+                }
+                else
+                {
+                    Console.Error.WriteLine($"Unable to find: {path}");
+                }
+            }
+            Console.Write("Any key to exit...");
+
+            _ = Console.ReadKey(true);
+        }
+    }
+}
